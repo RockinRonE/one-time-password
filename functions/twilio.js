f47ab6e7dfd0872edb7cd051dd5deb55f9512a3e
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-07-19 11:24:23 -0500

    Update twilio.js

--- functions/twilio.js
+++ functions/twilio.js
@@ -1,6 +1,6 @@
 const twilio = require('twilio');
 
-const accountSid = 'AC5580bad6b1835f61191902d4285cd91a';
-const authToken = 'c2f1cca39a211a53b0508b0b74b5c621';
+const accountSid = '';
+const authToken = '';
 
 module.exports = new twilio.Twilio(accountSid, authToken);
